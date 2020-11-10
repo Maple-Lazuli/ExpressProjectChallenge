86484e7d67da135fa2af3438092d17c25460f852
Maple-Lazuli <the.nostra.tymus@gmail.com>
2020-11-11 04:36:07 +0900

    fix to grades for student data

--- generateStudentData.js
+++ generateStudentData.js
@@ -1,12 +1,14 @@
 //Generate Data for the project :)
 const fs = require('fs');
 
-let numStudents = 10000
+let numStudents = 20000
 let majors = ['Mathematics', 'English', 'Lojban', 'Chemistry','Physics','Computer Science', 'Information Systems']
 let genders = ['Male', 'Female','Male', 'Female','Male', 'Female','Male', 'Female', 'Other'] //Make other genders less likely
 let year = ['1','1','1','1','1','2','2','2','3','3','4']  //Make higher year students less likely
 let firstNames = fs.readFileSync("fnames.txt").toString().split("\n")
 let lastNames =  fs.readFileSync("lnames.txt").toString().split("\n")
+
+//Fix case on last names
 fixedLastNames = lastNames.map(name =>{
      lame = name.toLowerCase()
      name = lame.charAt(0).toUpperCase() + lame.slice(1);
@@ -28,12 +30,14 @@ for(let i = 1; i < numStudents; i++){
     currentStudent = {...studentTemplete}
     currentStudent.firstName = firstNames[Math.floor((Math.random() * firstNames.length))]
     currentStudent.lastName = fixedLastNames[Math.floor((Math.random() * fixedLastNames.length))]
-    
+    currentStudent.grades =[]
+    for(let j = 0; j < 20;j++)
+        currentStudent.grades.push(Math.floor((Math.random() * (100-61)))+60)
     currentStudent.id = i
     currentStudent.major = majors[Math.floor((Math.random() * majors.length))]
-    currentStudent.gender = genders[Math.floor((Math.random() * genders.length))]
+    currentStudent.gender = genders[Math.floor((Math.random() * genders.length))]  //There is no correlation between name and gender here
     currentStudent.year = year[Math.floor((Math.random() * year.length))]
-    currentStudent.gpa = Math.floor((Math.random() * (100 - 51)) +  50)
+    currentStudent.gpa = currentStudent.grades.reduce((a,c) => a + c)/currentStudent.grades.length
     studentJSON.push(currentStudent)    
 }
 
